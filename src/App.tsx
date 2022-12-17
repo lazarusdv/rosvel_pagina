c5910533fccf3a0d9664428c786f49f489295b9c
ianPineda01 <ianpineda@ciencias.unam.mx>
2022-12-17 10:15:52 -0600

    Add basic information about the company

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+// import './components/Header'
+
 const App = () => {
-  return (
-    <div className="App">
-      Rosvel Servicios S. A. de C. V. <br/>
-      Todo en imanes y aplicaciones magneticas
-    </div>
-  );
+    return (
+        <div>
+            <p className="text-3xl font-bold">
+                Rosvel Servicios S. A. de C. V. <br/>
+                Todo en imanes y aplicaciones magneticas
+            </p>
+            <p>
+                Rosvel Servicios S.A de C.V <br />
+                Playa Mirador 556 <br />
+                Col. Reforma Iztaccihuatl Sur <br />
+                Del. Iztacalco, CP 08840 <br />
+                México, D.F.
+            </p>
+            <p>
+                55 5633 5782
+            </p>
+        </div>
+    )
 }
 
-export default App;
+export default App
